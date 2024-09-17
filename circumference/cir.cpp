00e44696be3de1cd61e20ae8972ca1376326ea60
Nasir Ali <160453804+nasirali84@users.noreply.github.com>
2024-09-17 22:46:39 +0530

    Add files via upload

---
+++ circumference/cir.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a =2;
+    int r= 10;
+    float pi = 3.14;
+    cout<<"The circumference of radius 10 is: "<<a*r*pi;
+
+    return 0;
+}
